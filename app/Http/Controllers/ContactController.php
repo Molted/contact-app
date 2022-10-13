42b97c650e3c4ba397e76a62b2d7dd85818f5135
Molted <joshuaconcepcion12.jc@gmail.com>
2022-10-13 21:36:55 +0800

    Added Controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index() {
+        $companies = [
+            1 => ['name' => 'Company One', 'contacts' => 3],
+            2 => ['name' => 'Company Two', 'contacts' => 5],
+        ];
+        $contacts = $this->getContacts();
+        // $contacts = []; // empty value
+        return view('contacts.index', compact('contacts', 'companies'));
+    }
+
+    public function create() {
+        return view('contacts.create'); // contacts = the folder, create = the file
+    }
+
+    public function show($id) {
+        $contacts = $this->getContacts();
+        abort_if(!isset($contacts[$id]), 404); //Validation for data in array of getContacts();
+        $contact = $contacts[$id];
+        return view('contacts.show')->with('contact', $contact);
+    }
+
+    protected function getContacts() {
+        return [
+            1 => ['name' => 'Name 1', 'phone' => '1234567890'],
+            2 => ['name' => 'Name 2', 'phone' => '2345678901'],
+            3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use Spatie\FlareClient\View;
 
@@ -14,41 +16,17 @@ use Spatie\FlareClient\View;
 |
 */
 
-function getContacts() {
-    return [
-        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
-    ];
-}
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // October 10, 2022 - Monday (First day of learning Laravel)
 // compact('contacts') is equivalent to ['contacts' => $contacts]
+// public function __invoke() - if there's only a single action on a controller -> singleAction controller
 
-Route::get('/contacts', function () {
-    $companies = [
-        1 => ['name' => 'Company One', 'contacts' => 3],
-        2 => ['name' => 'Company Two', 'contacts' => 5],
-    ];
-    $contacts = getContacts();
-    // $contacts = []; // empty value
-    return view('contacts.index', compact('contacts', 'companies'));
-})->name('contacts.index');
+Route::get('/', WelcomeController::class); // <- singleAction controller
 
-Route::get('/contacts/create', function () {
-    return view('contacts.create'); // contacts = the folder, create = the file
-})->name('contacts.create'); // to call route('contacts.create')
-
-Route::get('/contacts/{id}', function($id) {
-    $contacts = getContacts();
-    abort_if(!isset($contacts[$id]), 404); //Validation for data in array of getContacts();
-    $contact = $contacts[$id];
-    return view('contacts.show')->with('contact', $contact);
-})->whereNumber('id')->name('contacts.show'); // ->where('id', '[0-9]+');
+Route::controller(ContactController::class)->name('contacts.')->group(function() {
+    Route::get('/contacts', 'index')->name('index');
+    Route::get('/contacts/create', 'create')->name('create'); // to call route('contacts.create') in the View
+    Route::get('/contacts/{id}', 'show')->whereNumber('id')->name('show'); // ->where('id', '[0-9]+');
+});
 
 Route::get('/companies/{name?}', function($name = null) {
     if ($name){
