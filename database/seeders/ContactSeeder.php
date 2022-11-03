ec1e4a35e0fc45caaefc3c77ff1ea4f554882f87
Molted <joshuaconcepcion12.jc@gmail.com>
2022-11-03 18:49:55 +0800

    Inserted Fake data in Database

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -4,8 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Company;
+use App\Models\Contact;
+use Faker\Factory as Faker;
 
-class ContactsSeeder extends Seeder
+class ContactSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,6 +18,25 @@ class ContactsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $companies = Company::all();
+        $faker = Faker::create();
+        $contacts = [];
+
+        foreach ($companies as $company) {
+            foreach(range(1,5) as $index) {
+                $contact = [
+                    'first_name' => $faker->firstName(),
+                    'last_name' => $faker->lastName(),
+                    'phone' => $faker->phoneNumber(),
+                    'email' => $faker->email(),
+                    'address' => $faker->address(),
+                    'company_id' => $company->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+                $contacts[] = $contact;
+            }
+        }
+        Contact::insert($contacts);
     }
 }
