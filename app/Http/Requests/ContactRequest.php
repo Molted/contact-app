88a1e3cb2fd5ea0abf68632c1ec20a4dae16ba2c
Molted <joshuaconcepcion12.jc@gmail.com>
2022-11-09 21:43:35 +0800

    Form Request Validation

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -32,4 +32,20 @@ class ContactRequest extends FormRequest
             'company_id' => 'required|exists:companies,id'
         ];
     }
+
+    public function attributes()
+    {
+        return [
+            'company_id' => 'company',
+            'email' => 'email address'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.email' => 'The email that you entered is not valid',
+            '*.required' => ':attribute cannot be empty',
+        ];
+    }
 }
