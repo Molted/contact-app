db79e8e27590ee4cd7e8ce8227fa49e99ecd7bc1
Molted <joshuaconcepcion12.jc@gmail.com>
2022-11-09 13:52:14 +0800

    Styling Sortable Columns

--- app/helpers.php
+++ app/helpers.php
@@ -4,9 +4,15 @@ use Illuminate\Support\Str;
 
 function sortable($label, $column = null)
 {
-    $column = Str::snake($label);
-    $sortBy = request()->query('sort_by');
+    $column = $column ?? Str::snake($label); // check if the $column is null, if null then do Str:snake($label)
+    $sortBy = request()->query('sort_by'); // Default value is null
+    $direction = "";
+    if (ltrim($sortBy, "-") === $column)
+    {
+        $direction = strpos($sortBy, '-') === 0 ? 'desc' : 'asc';
+    }
     $sortBy = !$sortBy || strpos($sortBy, "-") === 0 ? $column : "-{$column}";
     $url = request()->fullUrlWithQuery(['sort_by' => $sortBy]);
-    return "<a href='{$url}'>{$label}</a>";
+    
+    return "<a href='{$url}' class='sortable {$direction}'>{$label}</a>";
 }
